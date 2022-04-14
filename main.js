0387e90de1266a54b7c0ab51f50b7451cec68605
Danny Kaump <dkaump92@gmail.com>
2022-04-14 15:44:31 -0700

    old solutions

--- main.js
+++ main.js
@@ -36,3 +36,52 @@ const multiplyAndFilter = (array, multiplier) =>
 
 // get evens from array
 const getEvenNumbers =(arr) => arr.filter(x => x % 2 === 0);
+
+// twice as old
+const twiceAsOld = (dadYearsOld, sonYearsOld) =>
+  Math.abs(dadYearsOld - sonYearsOld * 2);
+
+// remove every other element
+function removeEveryOther(arr){
+    let newArr = []
+    arr.forEach(function(x, i) { if (i % 2 === 0){
+      newArr.push(x)
+    }})
+    return newArr
+  }
+
+// freudian translator
+const toFreud = (string) => 
+  string ? string.split(' ').map(x => 'sex').join(' ') : string;
+
+// count instances of letter in str
+function strCount(str, letter){
+    let count = 0
+    str.split('').forEach(function(x) {
+      if (letter === x) {
+        count++
+      }
+    })
+    return count
+  }
+
+// basic calc
+function calculate(num1, operation, num2) {
+    switch (operation) {
+      case '+' :
+        return num1 + num2;
+      case '-' :
+        return num1 - num2;
+      case '/' :
+        return num2 === 0 ? null : num1 / num2;
+      case '*' :
+        return num1 * num2;
+      default :
+        return null
+   }
+  }
+
+//  create phone number 
+function createPhoneNumber(nums){
+    return `(${nums.slice(0, 3).join('')}) ${nums.slice(3, 6).join('')}-${nums.slice(6, 10).join('')}`
+  }
