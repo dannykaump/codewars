57439d47c80de822f4318331344228a7ee8b6a2b
Danny Kaump <dkaump92@gmail.com>
2022-05-04 12:10:17 -0700

    arrowfunc

--- removenthelement.js
+++ removenthelement.js
@@ -1,5 +1,7 @@
 // return array with nth element removed
 
-function removeNthElement(arr, n) {
-    return arr.slice(0, n).concat(arr.slice(n + 1))
-  }
+// function removeNthElement(arr, n) {
+//     return arr.slice(0, n).concat(arr.slice(n + 1))
+//   } 
+
+const removeNthElement = (arr, n) => [...arr.slice(0, n), ...arr.slice(n + 1)]
