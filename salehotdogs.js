0982dcb39205d989e4ff2a9f1558029a073bcf9d
Danny Kaump <dkaump92@gmail.com>
2022-05-04 19:33:54 -0700

    sale hot dogs

---
+++ salehotdogs.js
@@ -0,0 +1,17 @@
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
+
+// number	price (cents)
+// n < 5	100
+// n >= 5 and n < 10	95
+// n >= 10	90
+
+function saleHotdogs(n){
+    switch (true) {
+      case n < 5 :
+        return 100 * n;
+      case n < 10 :
+        return 95 * n;
+      default :
+        return 90 * n    
+    }
+  }
